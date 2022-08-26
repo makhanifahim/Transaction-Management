2973474dbbb2e225566922543c684298bf51cef3
Fahim <fahimmakhanifm@gmail.com>
2022-08-26 20:06:19 +0530

    custom metrics added
    custom metrics added for calculating the function time take to execute

--- src/main/java/TransactionMonitor/com/bbd/TransactionMonitorApplication.java
+++ src/main/java/TransactionMonitor/com/bbd/TransactionMonitorApplication.java
@@ -1,7 +1,10 @@
 package TransactionMonitor.com.bbd;
 
+import io.micrometer.core.aop.TimedAspect;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 //@Configuration
@@ -9,5 +12,10 @@ public class TransactionMonitorApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TransactionMonitorApplication.class, args);
+	}
+
+	@Bean
+	public TimedAspect timedAspect(MeterRegistry registry) {
+		return new TimedAspect(registry);
 	}
 }
