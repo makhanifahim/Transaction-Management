664ca7afe6875434eb08d8b3d52972c382509e6c
Fahim <fahimmakhanifm@gmail.com>
2022-08-26 18:53:08 +0530

    open api doc and actuator added

--- src/main/java/TransactionMonitor/com/bbd/TransactionMonitorApplication.java
+++ src/main/java/TransactionMonitor/com/bbd/TransactionMonitorApplication.java
@@ -1,15 +1,10 @@
 package TransactionMonitor.com.bbd;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableSwagger2
-@Configuration
-@Slf4j
+//@Configuration
 public class TransactionMonitorApplication {
 
 	public static void main(String[] args) {
